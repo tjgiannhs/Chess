cd99bc0acb2a2b52f6488a280480ba1b52cbd684
DESKTOP-MH3J6QE\tjgia <tjgiannhs8es@gmail.com>
2022-06-22 18:52:38 +0300

    Added all local Chess project files

---
+++ Chess/Assets/GamePiece.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePiece : MonoBehaviour
+{
+    public enum PieceType {Pawn, Bishop, Knight, Rook, Queen, King};
+
+    [SerializeField] PieceType pieceType;
+    [SerializeField] int pieceTeam;
+    [SerializeField] int pieceValue;
+    [SerializeField] int timesMoved=0;
+    [SerializeField] int timesAte=0;
+    [SerializeField] int profit=0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public PieceType GetPieceType()
+    {
+        return pieceType;
+    }
+
+    public int GetPieceTeam()
+    {
+        return pieceTeam;
+    }
+
+    public void SetPieceTeam(int i, Color32 c)
+    {
+        pieceTeam = i;
+        GetComponent<Image>().color = c;
+    }
+
+    public int GetTimesMoved()
+    {
+        return timesMoved;
+    }
+    public int GetTimesEaten()
+    {
+        return timesAte;
+    }
+    public int GetProfit()
+    {
+        return profit;
+    }
+    public int GetPieceValue()
+    {
+        return pieceValue;
+    }
+
+    public void increaseTimesMoved(int i = 0)
+    {
+        timesMoved+=i;
+    }
+    public void increaseTimesAte(int i = 0)
+    {
+        timesAte+=i;
+    }
+    public void increasePieceProfit(int i = 0)
+    {
+        profit+=i;
+    }
+}
